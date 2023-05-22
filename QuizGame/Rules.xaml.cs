5e75153d10d02eb435aa0738d5ae1d92c7ff1c7b
scffs <67464545+scffs@users.noreply.github.com>
2023-05-22 14:23:19 +0700

    changed the logic of rules

--- QuizGame/Rules.xaml.cs
+++ QuizGame/Rules.xaml.cs
@@ -1,32 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace QuizGame
 {
-    /// <summary>
-    /// Логика взаимодействия для Rules.xaml
-    /// </summary>
     public partial class Rules : Window
     {
+        private readonly string[] rules;
+        private int counter;
+
         public Rules()
         {
             InitializeComponent();
             ruleButtonBefore.IsEnabled = false;
-        }
-        int counter;
 
-        readonly string[] rules = 
+            rules = new string[]
             {
                 "В качестве ответа надо ввести предложение, в котором все изображения заменены на предполагаемые слова",
                 "Количество времени зависит от сложности (2-5 минут). По умолчанию - 3",
@@ -34,29 +20,30 @@ namespace QuizGame
                 "Вы можете выбрать количество пиктограмм (от 2 до 5). По умолчанию - 3",
                 "Удачной игры!"
             };
+
+            counter = 0;
+            UpdateRuleText();
+        }
+
         private void NextRule(object sender, RoutedEventArgs e)
         {
-            if (counter == 1) ruleButtonBefore.IsEnabled = true;
-            if (counter + 1 == rules.Length) ruleButtonNext.IsEnabled = false;
-            else ruleButtonNext.IsEnabled = true;
-            rulesBlock.Text = $" [{counter + 1}/" + rules.Length + "] " + rules[counter];
             counter++;
+            UpdateRuleText();
         }
+
         private void PrevRule(object sender, RoutedEventArgs e)
         {
             counter--;
-            if (counter - 1 <= 0)
-            {
-                ruleButtonBefore.IsEnabled = false;
-                ruleButtonNext.IsEnabled = true;
-            }
-            else
-            {
-                ruleButtonBefore.IsEnabled = true;
-                ruleButtonNext.IsEnabled = true;
-            }
-            rulesBlock.Text = $" [{counter}/" + rules.Length + "] " + rules[counter - 1];
+            UpdateRuleText();
         }
-        public void CloseWindow(object sender, RoutedEventArgs e) => Close();
+
+        private void UpdateRuleText()
+        {
+            ruleButtonBefore.IsEnabled = counter > 0;
+            ruleButtonNext.IsEnabled = counter < rules.Length - 1;
+            rulesBlock.Text = $" [{counter + 1}/{rules.Length}] {rules[counter]}";
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e) => Close();
     }
 }
