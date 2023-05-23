2382ec948f92b83c010702caa638129aa2c664ce
scffs <67464545+scffs@users.noreply.github.com>
2023-05-23 19:31:03 +0700

    Rebuild и очистка проекта

--- QuizGame/GameInitializer.cs
+++ QuizGame/GameInitializer.cs
@@ -1,7 +1,7 @@
-﻿using System.Windows.Media.Imaging;
-using System.Windows.Media;
-using System;
+﻿using System;
 using System.IO;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 
 namespace QuizGame
 {
