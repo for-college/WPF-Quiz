29ca51fabdbe901402faa6afad5a2daa82e8b901
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2023-05-23 10:03:48 +0700

    added questions

--- QuizGame/MainWindow.xaml.cs
+++ QuizGame/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Media;
 using System.Windows.Media.Imaging;
 
 namespace QuizGame
@@ -20,19 +21,61 @@ namespace QuizGame
             controller.DisplayCurrentQuestion();
         }
 
-        private void InitializeWordImageMap()
+        private ImageSource GetImageSource(string imagePath)
         {
-            WordImageMap wordImageMap = new WordImageMap();
-            string imagePath = "me.png"; // Относительный путь к картинке
-
             BitmapImage imageSource = new BitmapImage();
             imageSource.BeginInit();
             imageSource.UriSource = new Uri(imagePath, UriKind.Relative);
             imageSource.EndInit();
 
-            wordImageMap.AddWordImagePair("apple", imageSource);
-            wordImageMap.AddWordImagePair("banana", imageSource);
-            wordImageMap.AddWordImagePair("orange", imageSource);
+            return imageSource;
+        }
+
+        private void InitializeWordImageMap()
+        {
+            WordImageMap wordImageMap = new WordImageMap();
+
+            // TODO: Придумать как это вынести куда-то
+            // TODO: В каких-то предложениях слишком мало пиктограмм
+
+            string[] sentencesImages = { 
+                // 1
+                "me.png", 
+                "birch.png", 
+                "under.png",
+                "window.png",
+                // 2
+                "guys.png",
+                "river.png",
+                "swimming.png",
+                // 3
+                "berries.png",
+                "mushrooms.png",
+                // 4
+                "basketball.png",
+                "football.png",
+                "hockey.png",
+                "running.png",
+                "volleyball.png",
+                // 5
+                "rain.png",
+                "rainbow.png",
+                "stop.png",
+                "wait.png",
+                // 6
+                "fog.png",
+                "look.png",
+                "us.png",
+                // 7
+                "apple.png",
+                "pears.png", 
+            };
+            foreach (string imageName in sentencesImages)
+            {
+                // Сплитим до по точке и берём первый элемент (для словаря)
+                string word = imageName.Split('.')[0];
+                wordImageMap.AddWordImagePair(word, GetImageSource(imageName));
+            }
 
             // Создать контроллер с передачей wordImageMap
             Model model = new Model();
@@ -41,9 +84,14 @@ namespace QuizGame
 
         private void InitializeQuestions()
         {
-            // Добавьте несколько вопросов
-            controller.AddQuestion("I have a [apple] and an [orange]", "I have a apple and an orange");
-            controller.AddQuestion("She likes [banana]", "She likes bananas");
+            // Добавление вопросов
+            controller.AddQuestion("Белая [birch] [under] [me] [window]", "Белая береза под моим окном");
+            controller.AddQuestion("[guys] [swimming] в [river]", "Мальчики купаются в речке");
+            controller.AddQuestion("Завтра утром [me] пойду в лес за [mushrooms] и [berries]", "Завтра утром я пойду в лес за грибами и ягодами");
+            controller.AddQuestion("Гена перечислил известные виды спорта: [football] , [basketball] , [volleyball] , [hockey] , [running]", "Гена перечислил известные виды спорта: футбол, баскетбол, волейбол, хоккей, бег");
+            controller.AddQuestion("После [rain] дети [wait] [rainbow] , но [rain] не собирался [stop]", "После дождя дети ждали радугу, но дождь не собирался останавливаться");
+            controller.AddQuestion("[us] [look] овраг лишь тогда, когда рассеялся [fog]", "Мы увидели овраг лишь тогда, когда рассеялся туман");
+            controller.AddQuestion("После [rain] дети [wait] [rainbow] , но [rain] не собирался [stop]", "После дождя дети ждали радугу, но дождь не собирался останавливаться");
         }
 
         private void SubmitAnswerButton_Click(object sender, RoutedEventArgs e)
