34ba89360d44ceeb62f0aafc14f2d2c4d5823fbe
Semyon Okulov <67464545+scffs@users.noreply.github.com>
2023-05-23 12:50:11 +0700

    Добавлен класс для хранения данных

--- QuizGame/GameInitializer.cs
+++ QuizGame/GameInitializer.cs
@@ -7,6 +7,8 @@ namespace QuizGame
     public class GameInitializer
     {
         private MainWindow mainWindow;
+        
+        QuizData quizData = new QuizData();
 
         public GameInitializer(MainWindow mainWindow)
         {
@@ -16,7 +18,7 @@ namespace QuizGame
         public void Initialize()
         {
             WordImageMap wordImageMap = InitializeWordImageMap();
-            Model model = InitializeModel(wordImageMap);
+            Model model = InitializeModel();
 
             View view = new View(mainWindow);
             Controller controller = new Controller(model, wordImageMap, view);
@@ -42,38 +44,8 @@ namespace QuizGame
         {
             WordImageMap wordImageMap = new WordImageMap();
 
-            string[] sentencesImages = { 
-                // 1
-                "me.png",
-                "birch.png",
-                "under.png",
-                "window.png",
-                // 2
-                "guys.png",
-                "river.png",
-                "swimming.png",
-                // 3
-                "berries.png",
-                "mushrooms.png",
-                // 4
-                "basketball.png",
-                "football.png",
-                "hockey.png",
-                "running.png",
-                "volleyball.png",
-                // 5
-                "rain.png",
-                "rainbow.png",
-                "stop.png",
-                "wait.png",
-                // 6
-                "fog.png",
-                "look.png",
-                "us.png",
-                // 7
-                "apple.png",
-                "pears.png",
-            };
+            string[] sentencesImages = quizData.SentencesImages;
+            
             foreach (string imageName in sentencesImages)
             {
                 // Сплитим до по точке и берём первый элемент (для словаря)
@@ -84,18 +56,14 @@ namespace QuizGame
             return wordImageMap;
         }
 
-        private Model InitializeModel(WordImageMap wordImageMap)
+        private Model InitializeModel()
         {
             Model model = new Model();
 
-            // Добавление вопросов в модель
-            model.AddQuestion("Белая [birch] [under] [me] [window]", "Белая береза под моим окном");
-            model.AddQuestion("[guys] [swimming] в [river]", "Мальчики купаются в речке");
-            model.AddQuestion("Завтра утром [me] пойду в лес за [mushrooms] и [berries]", "Завтра утром я пойду в лес за грибами и ягодами");
-            model.AddQuestion("Гена перечислил известные виды спорта: [football] , [basketball] , [volleyball] , [hockey] , [running]", "Гена перечислил известные виды спорта: футбол, баскетбол, волейбол, хоккей, бег");
-            model.AddQuestion("После [rain] дети [wait] [rainbow] , но [rain] не собирался [stop]", "После дождя дети ждали радугу, но дождь не собирался останавливаться");
-            model.AddQuestion("[us] [look] овраг лишь тогда, когда рассеялся [fog]", "Мы увидели овраг лишь тогда, когда рассеялся туман");
-            model.AddQuestion("После [rain] дети [wait] [rainbow] , но [rain] не собирался [stop]", "После дождя дети ждали радугу, но дождь не собирался останавливаться");
+            for (int i = 0; i < quizData.Questions.Length; i++)
+            {
+                model.AddQuestion(quizData.Questions[i], quizData.Answers[i]);
+            }
 
             return model;
         }
---
+++ QuizGame/QuizData.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizGame
+{
+    public class QuizData
+    {
+        public string[] Questions { get; } = {
+        "Белая [birch] [under] [me] [window]",
+        "[guys] [swimming] в [river]",
+        "Завтра утром [me] пойду в лес за [mushrooms] и [berries]",
+        "Гена перечислил известные виды спорта: [football] , [basketball] , [volleyball] , [hockey] , [running]",
+        "После [rain] дети [wait] [rainbow] , но [rain] не собирался [stop]",
+        "[us] [look] овраг лишь тогда, когда рассеялся [fog]",
+        "После [rain] дети [wait] [rainbow] , но [rain] не собирался [stop]"
+        };
+
+        public string[] Answers { get; } = {
+        "Белая береза под моим окном",
+        "Мальчики купаются в речке",
+        "Завтра утром я пойду в лес за грибами и ягодами",
+        "Гена перечислил известные виды спорта: футбол, баскетбол, волейбол, хоккей, бег",
+        "После дождя дети ждали радугу, но дождь не собирался останавливаться",
+        "Мы увидели овраг лишь тогда, когда рассеялся туман",
+        "После дождя дети ждали радугу, но дождь не собирался останавливаться"
+        };
+
+        public string[] SentencesImages { get; } = {
+        // 1
+        "me.png",
+        "birch.png",
+        "under.png",
+        "window.png",
+        // 2
+        "guys.png",
+        "river.png",
+        "swimming.png",
+        // 3
+        "berries.png",
+        "mushrooms.png",
+        // 4
+        "basketball.png",
+        "football.png",
+        "hockey.png",
+        "running.png",
+        "volleyball.png",
+        // 5
+        "rain.png",
+        "rainbow.png",
+        "stop.png",
+        "wait.png",
+        // 6
+        "fog.png",
+        "look.png",
+        "us.png",
+        // 7
+        "apple.png",
+        "pears.png",
+        };
+    }
+}
